10fa0ef9d7d6ed4df43d556926a83c8744ca8be7
liubolif <lyubomyrif@gmail.com>
2020-11-30 14:23:28 +0200

    update after lab12

--- test/mailers/order_mailer_test.rb
+++ test/mailers/order_mailer_test.rb
@@ -6,16 +6,14 @@ class OrderMailerTest < ActionMailer::TestCase
     assert_equal "Velostore. Підтвердження замовлення", mail.subject
     assert_equal ["lyubomyrif@gmail.com"], mail.to
     assert_equal ["fatsiievychif@gmail.com"], mail.from
-    assert_match /1 x Велосипед Cannondale TOPSTONE 2 2020/, mail.body.encoded
+    assert_match /1 x Велосипед Comanche Tomahawk/, mail.body.encoded
   end
-
   test "shipped" do
     mail = OrderMailer.shipped(orders(:one))
     assert_equal "Velostore. Замовлення відвантажене.", mail.subject
     assert_equal ["lyubomyrif@gmail.com"], mail.to
     assert_equal ["fatsiievychif@gmail.com"], mail.from
-    assert_match /<td[^>]*>1<\/td>\s*<td>Велосипед Cannondale TOPSTONE 2 2020<\/td>/,
+    assert_match /<td[^>]*>1<\/td>\s*<td>Велосипед Comanche Tomahawk<\/td>/,
                  mail.body.encoded
   end
-
 end
